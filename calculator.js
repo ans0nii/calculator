0cfda3bbda187acdbaaf5c502fafa4816e1e6888
Anthony <anthonys2u2@gmail.com>
2025-09-02 16:43:26 -0500

    Add actual numbers values and create object to handle interchanged array

--- calculator.js
+++ calculator.js
@@ -4,13 +4,25 @@ document.querySelectorAll(".calc-button").forEach((element) => {
     let display = document.querySelector("#number-input");
     let currentValue = display.value;
     let newValue = currentValue + buttonValue;
+    const strNumbers = currentValue.split(/[*+/=-c%]/);
+    const operators = currentValue.match(/[*+/=-c%]/g);
+
+    console.log(strNumbers);
+    console.log(operators);
 
     if (currentValue.length < 9) {
       newValue = currentValue + buttonValue;
       display.value = buttonValue;
       display.value = newValue;
     }
-    
-    console.log(buttonValue);
+    let actualNumber = strNumbers;
+    actualNumber = strNumbers.map(Number);
+
+    const tokenArray = actualNumber.map((num, i) => {
+      return {
+        value: num,
+        operator: operators[i] || null,
+      };
+    });
   });
 });
