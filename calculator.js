4d8768380c509b0a9119611854a39e267c425ccb
Anthony <anthonys2u2@gmail.com>
2025-09-02 13:16:18 -0500

    Add styling and functional number display

--- calculator.js
+++ calculator.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+document.querySelectorAll(".calc-button").forEach((element) => {
+  element.addEventListener("click", (e) => {
+    let buttonValue = e.target.textContent;
+    let display = document.querySelector("#number-input");
+    let currentValue = display.value;
+    let newValue = currentValue + buttonValue;
+
+    if (currentValue.length < 9) {
+      newValue = currentValue + buttonValue;
+      display.value = buttonValue;
+      display.value = newValue;
+    }
+    
+    console.log(buttonValue);
+  });
+});
